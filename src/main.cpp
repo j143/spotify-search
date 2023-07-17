59632f1213de6f619b204b218738801e5654156d
Janardhan Pulivarthi <j143@protonmail.com>
2023-07-17 01:10:56 +0000

    try using the sample code

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include <curl/curl.h>
+
 #include <spotify/json.hpp>
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output) {
-    size_t totalSize = size * nmemb;
-    output->append(static_cast<char*>(contents), totalSize);
-    return totalSize;
-}
+using namespace spotify::json;
 
-int main() {
-    std::string songName;
-    std::cout << "Enter the name of a song: ";
-    std::getline(std::cin, songName);
+struct Track {
+  std::string uri;
+  std::string uid;
+  std::map<std::string, std::string> metadata;
+};
 
-    std::string queryUrl = "https://api.spotify.com/v1/search?q=" + songName + "&type=track";
+namespace spotify {
+namespace json {
 
-    curl_global_init(CURL_GLOBAL_ALL);
-    CURL* curl = curl_easy_init();
+// Specialize spotify::json::default_codec_t to specify default behavior when
+// encoding and decoding objects of certain types.
+template <>
+struct default_codec_t<Track> {
+  static object_t<Track> codec() {
+    auto codec = object<Track>();
+    codec.required("uri", &Track::uri);
+    codec.optional("uid", &Track::uid);
+    codec.optional("metadata", &Track::metadata);
+    return codec;
+  }
+};
 
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, queryUrl.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+}  // namespace json
+}  // namespace spotify
 
-        std::string response;
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+int main() {
+  const auto parsed_track = decode<Track>(R"({ "uri": "spotify:track:xyz", "metadata": { "a": "b" } })");
+  std::cout << "Parsed track with uri " << parsed_track.uri << std::endl;
 
-        CURLcode res = curl_easy_perform(curl);
+  Track track;
+  track.uri = "spotify:track:abc";
+  track.uid = "a-uid";
+  const auto json = encode(track);
+  std::cout << "Encoded the track into " << json << std::endl;
 
-        if (res == CURLE_OK) {
-            try {
-                spotify::json::value json = spotify::json::parse_string(response);
-
-                // Process the JSON data and display the desired information
-                // For example, you can display the first result's name and artist
-                const auto& tracks = json["tracks"]["items"];
-                if (tracks.is_array() && !tracks.empty()) {
-                    const auto& track = tracks[0];
-                    std::string trackName = track["name"].get<std::string>();
-                    std::string artistName = track["artists"][0]["name"].get<std::string>();
-
-                    std::cout << "Track: " << trackName << std::endl;
-                    std::cout << "Artist: " << artistName << std::endl;
-                } else {
-                    std::cout << "No results found for the song." << std::endl;
-                }
-            } catch (const std::exception& e) {
-                std::cerr << "Error parsing JSON response: " << e.what() << std::endl;
-            }
-        } else {
-            std::cerr << "Failed to perform HTTP request: " << curl_easy_strerror(res) << std::endl;
-        }
-
-        curl_easy_cleanup(curl);
-    } else {
-        std::cerr << "Failed to initialize curl" << std::endl;
-    }
-
-    curl_global_cleanup();
-
-    return 0;
+  return 0;
 }
